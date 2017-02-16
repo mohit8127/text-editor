3d73a24ca2d2943372ff19983fd0feb6e705154c
rahulworld <chauhanr371@gmail.com>
2017-02-17 02:04:35 +0530

    added screenshots

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # text-editor
+
+#### A Simple Tk-inter Gui App: 
+<img src="/screenshots/texteditor.png" width="250px">
--- TextEditor.py
+++ TextEditor.py
@@ -1,13 +1,16 @@
 import Tkinter
 from Tkinter import *
+#import Scrolled because tk has not this scrolling.
 from ScrolledText import *
 import tkFileDialog
 import tkMessageBox
 
 root = Tkinter.Tk(className="Text Editor")
 textPad = ScrolledText(root, width=100, height=80)
+# we adding fundamental feature in text editor
 # create a menu & define functions for each menu item
 
+# define open command in menu
 def open_command():
     file = tkFileDialog.askopenfile(parent=root, mode='rb', title='Select a file')
     if file != None:
@@ -15,7 +18,7 @@ def open_command():
         textPad.insert('1.0', contents)
         file.close()
 
-
+# define save command in menu
 def save_command(self):
     file = tkFileDialog.asksaveasfile(mode='w')
     if file != None:
@@ -24,12 +27,12 @@ def save_command(self):
         file.write(data)
         file.close()
 
-
+# define exit command in menu
 def exit_command():
     if tkMessageBox.askokcancel("Quit", "Do you really want to quit?"):
         root.destroy()
 
-
+# define about command in menu
 def about_command():
     label = tkMessageBox.showinfo("About", "Rahul Chauhan")
 
